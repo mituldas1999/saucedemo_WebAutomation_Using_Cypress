f5fa41be46bd20a38a50cc5df7431de85027d9cf
Mitul Das <mituldas751@gmail.com>
2025-02-11 00:29:58 +0600

    Test Reporting

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
+    screenshotsFolder: "cypress/screenshots", 
+    screenshotOnRunFailure: true, // Capture screenshot on test failure
+    videosFolder: "cypress/videos", // Folder for videos
+    video: true,
   },
 });
